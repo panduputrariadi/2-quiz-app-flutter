0d203922413e37a4f73ed56d2bd1251d3aae8ce7
panduputragit <put.rariadi1144@gmail.com>
2025-02-02 18:44:51 +0800

    call dummy data on model

--- lib/question_quiz.dart
+++ lib/question_quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/components/answer_button.dart';
+import 'package:quiz_app/data/question.dart';
 
 class QuestionQuiz extends StatefulWidget {
   const QuestionQuiz({super.key});
@@ -12,15 +13,16 @@ class QuestionQuiz extends StatefulWidget {
 class _QuestionQuizState extends State<QuestionQuiz> {
   @override
   Widget build(context) {
+    final dataArrayQuestion = Question[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question',
-            style: TextStyle(
-              fontSize: 28,
+          Text(
+            dataArrayQuestion.text,
+            style: const TextStyle(
+              fontSize: 15,
               color: Colors.white,
             ),
           ),
@@ -28,15 +30,19 @@ class _QuestionQuizState extends State<QuestionQuiz> {
             height: 30,
           ),
           AnswerButton(
-            answerText: 'Answer 1',
+            answerText: dataArrayQuestion.answer[0],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: 'Answer 2',
+            answerText: dataArrayQuestion.answer[1],
             onTap: () {},
           ),
           AnswerButton(
-            answerText: 'Answer 3',
+            answerText: dataArrayQuestion.answer[2],
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: dataArrayQuestion.answer[3],
             onTap: () {},
           ),
         ],
